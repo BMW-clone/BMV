34a4e55222b282d3e1eef3304d13c08ab1a9f5c6
Salami <medselimthabet2@gmail.com>
2023-07-19 18:05:34 +0100

    finished SignUp & SignIn

---
+++ bmw/app/SignUp/page.tsx
@@ -0,0 +1,128 @@
+"use client"
+import React, { useState } from 'react'
+import axios, { AxiosError, AxiosResponse } from 'axios'
+import { useRouter } from 'next/navigation'
+import "./SignUp.css"
+const SignUp = () => {
+
+    const [username, setUsername] = useState<string>("")
+    const [email, setEmail] = useState<string>("")
+    const [firstname, setFirstname] = useState<string>("")
+    const [lastname, setLastName] = useState<string>("")
+    const [password, setPassword] = useState<string>("")
+    const [profilepic, setProfilePic] = useState<string>("https://res.cloudinary.com/dhz4wb76m/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1688671165/profile_images/qdq7bnpm9lqqbo3ufmc9.jpg?_s=public-apps")
+    const [role, setRole] = useState<string>("")
+    const [phoneNumber, setPhoneNumber] = useState<string>("")
+    const [coverpic, setCoverPic] = useState<string>("https://notepd.s3.amazonaws.com/default-cover.png")
+    const router = useRouter()
+    //!signup function for client
+    const handleSingUpClient = () => {
+        axios.post("http://localhost:5000/client/signup", { firstname, lastname, username, email, password, profilepic, role, phoneNumber, coverpic })
+            .then((res: AxiosResponse) => console.log(res))
+            .catch((err: AxiosError) => console.log(err))
+    }
+    //!signup function for seller
+    const handleSingUpSeller = () => {
+        axios.post("http://localhost:5000/seller/signup", { firstname, lastname, username, email, password, profilepic, role, phoneNumber, coverpic })
+            .then((res: AxiosResponse) => console.log(res))
+            .catch((err: AxiosError) => console.log(err))
+    }
+
+    return (
+        <div className="sign-up-sp">
+            <div className="sign-up-child-sp" />
+            <div className="rectangle-parent-sp">
+                <div className="group-child-sp" />
+                <b className="begin-your-meta-sp">Begin your BMW  journey here</b>
+                <div
+                    className="already-a-member-container-sp"
+                    onClick={() => router.push("/")}
+                >
+                    <span>
+                        <span>Already a Member?</span>
+                        <span className="span-sp" >{` `}</span>
+                    </span>
+                    <span className="span-sp">
+                        <span className="sign-in1-sp">Sign In</span>
+                    </span>
+                </div>
+                <div className="group-parent-sp">
+                    <div className="full-name-parent-sp2">
+                        <div className="group">
+                            <input type="text" required onChange={(e) => setEmail(e.target.value)} />
+                            <span className="highlight"></span>
+                            <span className="bar"></span>
+                            <label>Email</label>
+                        </div>
+                    </div>
+                    <div className="last-name-parent-sp2">
+                        <div className="group">
+                            <input type="text" required onChange={(e) => setUsername(e.target.value)} />
+                            <span className="highlight"></span>
+                            <span className="bar"></span>
+                            <label>Username</label>
+                        </div>
+                    </div>
+                </div>
+                <div className="vector-parent-sp">
+                    <div className="group">
+                        <input type="password" required onChange={(e) => setPassword(e.target.value)} />
+                        <span className="highlight"></span>
+                        <span className="bar"></span>
+                        <label>Password</label>
+                    </div>
+                </div>
+                <div className="vector-parent-sp2">
+                    <div className="group">
+                        <input type="text" required onChange={(e) => setPhoneNumber(e.target.value)} />
+                        <span className="highlight"></span>
+                        <span className="bar"></span>
+                        <label>Phone Number</label>
+                    </div>
+                </div>
+                <div className="group-parent-sp">
+                    <div className="full-name-parent-sp">
+                        <div className="group">
+                            <input type="text" required onChange={(e) => setFirstname(e.target.value)} />
+                            <span className="highlight"></span>
+                            <span className="bar"></span>
+                            <label>First Name</label>
+                        </div>
+                    </div>
+                    <div className="last-name-parent-sp">
+                        <div className="group">
+                            <input type="text" required onChange={(e) => setLastName(e.target.value)} />
+                            <span className="highlight"></span>
+                            <span className="bar"></span>
+                            <label>Last Name</label>
+                        </div>
+
+                    </div>
+                </div>
+                <div className="selectdiv">
+                    <select onChange={(e) => { setRole(e.target.value); }} >
+                        <option selected disabled> Select Account Type </option>
+                        <option defaultValue="client">Client</option>
+                        <option defaultValue="seller">Seller</option>
+                    </select>
+
+                </div>
+                <div className="create-account-wrapper-sp" onClick={() => {
+                    if (role === "Client") {
+                        handleSingUpClient()
+                        router.push("/")
+                    } else {
+                        handleSingUpSeller()
+                        router.push("/")
+                    }
+                }}>
+                    <div className="create-account-sp">Create Account</div>
+                </div>
+            </div>
+            <img className="image-8-icon-sp" alt="" src="https://res.cloudinary.com/dhz4wb76m/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1689289934/bmw_logo_kp6bvt.jpg?_s=public-apps" />
+            <div className="sign-up1-sp">Sign Up</div>
+        </div >
+    );
+};
+
+export default SignUp
--- bmw/app/page.tsx
+++ bmw/app/page.tsx
@@ -1,94 +1,140 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-export default function Home() {
+"use client"
+import axios, { AxiosError, AxiosResponse } from 'axios'
+import jwtDecoder from "jwt-decode";
+import Cookies from "universal-cookie";
+import React, { useState, FC, useEffect } from 'react';
+import { useRouter } from 'next/navigation'
+import "./SignIn.css";
+import Image from 'next/image';
+
+//!interfaces
+interface Decoded {
+  username: string,
+  password: string,
+  role: string,
+  profilepic: string
+  exp: number,
+}
+//!----------------------
+
+const Login: FC = () => {
+  useEffect(() => {
+    const logout = () => {
+      let cookie = new Cookies()
+      cookie.remove("jwt-token")
+    }
+    logout()
+  }, [])
+  const router = useRouter()
+  //! initialize cookies
+  const cookies = new Cookies()
+  //!logged user info
+  const [user, setUser] = useState<object | null>(null)
+  //!entered login info
+  const [username, setUsername] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  //!logout function
+  const logout = () => {
+    setUser(null)
+    cookies.remove("jwt-token")
+  }
+  //!login function
+  const login = () => {
+    axios.post("http://localhost:5000/client/login", { username, password })
+      .then((res: AxiosResponse) => {
+        console.log("res", res.data);
+        if (res.data) {
+          //!decoding token
+          const decoded: Decoded = jwtDecoder(res.data)
+          //!setting data to logged user
+          setUser(decoded)
+          //! setting data to cookie
+          cookies.set("jwt-token", res.data, {
+            expires: new Date(decoded.exp * 1000)
+
+          })
+          router.push("/UserProfile")
+        }
+      }).catch((err: AxiosError) => {
+        if (err.response?.data === "user not found") {
+          axios.post("http://localhost:5000/seller/login", { username, password })
+            .then((res: AxiosResponse) => {
+              console.log("data", res.data);
+              if (res.data) {
+                //!decoding token
+                const decoded: Decoded = jwtDecoder(res.data)
+                console.log("decoded", decoded);
+                //!setting data to logged user
+                setUser(decoded)
+                //! setting data to cookie
+                cookies.set("jwt-token", res.data, {
+                  expires: new Date(decoded.exp * 1000)
+                })
+                router.push("/SellerProfile")
+              }
+            }).catch((err: AxiosError) => {
+              if (err.response?.data === "user not found") {
+
+                axios.post("http://localhost:5000/admin/login", { username, password })
+                  .then((res: AxiosResponse) => {
+                    if (res.data) {
+                      //!decoding token
+                      const decoded: Decoded = jwtDecoder(res.data)
+                      //!setting data to logged user
+                      setUser(decoded)
+                      //! setting data to cookie
+                      cookies.set("jwt-token", res.data, {
+                        expires: new Date(decoded.exp * 1000)
+                      })
+                      router.push("/Dashboard")
+                    }
+                  }).catch((err: AxiosError) => {
+                    console.log(err)
+                  })
+              }
+            })
+        }
+      })
+  }
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          HELLO IM WORKING&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <div className="sign-in-si">
+        <div className="sign-in-child-si" />
+        <div className="rectangle-parent-si">
+          <div className="group-child-si" />
+          <b className="explore-the-world-si">Explore the world of BMW</b>
+          <div style={{ textDecoration: "none" }}
+            className="new-user-create-container-si"
+            onClick={() => router.push("/SignUp")}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <span style={{ color: 'white' }}>New user?</span>
+            <span className="create-an-account-si"> Create an account</span>
+          </div>
+          <div className="email-address-parent-si">
+            <div className="group-si">
+              <input type="text" required onChange={(e) => setUsername(e.target.value)} />
+              <span className="highlight-si"></span>
+              <span className="bar-si"></span>
+              <label>Username</label>
+            </div>
+            <div className="group-si">
+              <input type="password" required onChange={(e) => setPassword(e.target.value)} />
+              <span className="highlight-si"></span>
+              <span className="bar-si"></span>
+              <label>Password</label>
+            </div>
+          </div>
+        </div>
+        <img className="image-8-icon-si" alt="" src="https://res.cloudinary.com/dhz4wb76m/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1689289934/bmw_logo_kp6bvt.jpg?_s=public-apps" />
+        <div className="sign-in1-si">Sign In</div>
+        <div className="continue-wrapper-si" onClick={() => {
+          login()
+        }}>
+          <div className="continue-si">Continue</div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
+
+export default Login
