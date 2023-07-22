e3ceff2f7c7f5c66bc738ae9ed5171fc983fd456
Salami <medselimthabet2@gmail.com>
2023-07-22 20:46:05 +0100

    flouci linked to checkout button

--- bmw/app/Payment/page.tsx
+++ bmw/app/Payment/page.tsx
@@ -1,29 +1,29 @@
-"use client"
+// "use client"
 
-import React, { FC, useState } from 'react'
-import axios, { AxiosError, AxiosResponse } from 'axios'
-const payment: FC = () => {
-    const [amount, setAmount] = useState<string>("")
-    const handleCheckout = () => {
-        axios.post("http://localhost:5000/flouci/payment", { amount: Number(amount) })
-            .then((res: AxiosResponse) => {
-                const { result } = res.data
-                console.log(result);
+// import React, { FC, useState } from 'react'
+// import axios, { AxiosError, AxiosResponse } from 'axios'
+// const payment: FC = () => {
+//     const [amount, setAmount] = useState<string>("")
+//     const handleCheckout = () => {
+//         axios.post("http://localhost:5000/flouci/payment", { amount: Number(amount) })
+//             .then((res: AxiosResponse) => {
+//                 const { result } = res.data
+//                 console.log(result);
 
-                window.location.replace(result.link)
-            })
-            .catch((err: AxiosError) => console.log(err))
+//                 window.location.replace(result.link)
+//             })
+//             .catch((err: AxiosError) => console.log(err))
 
-    }
-    return (
-        <div>
-            <center>
-                <input type="text" onChange={(e) => setAmount(e.target.value)} />
-                <button onClick={handleCheckout}>Checkout</button>
-            </center>
+//     }
+//     return (
+//         <div>
+//             <center>
+//                 <input type="text" onChange={(e) => setAmount(e.target.value)} />
+//                 <button onClick={handleCheckout}>Checkout</button>
+//             </center>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default payment
+// export default payment
--- bmw/app/cart/page.tsx
+++ bmw/app/cart/page.tsx
@@ -1,33 +1,51 @@
-import React from 'react'
+"use client"
+import React, { FC, useState } from 'react'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
 interface Car {
-    id:number;
-    brand:string;
-    price:number;
-    category:string;
-    color:string;
-    year:number;
-    image:string;
-    mileage:number;
-    model:string;
-    transmission:string;
-    hp:number;
-    carburant:string;
-    rate:number;
+  id: number;
+  brand: string;
+  price: number;
+  category: string;
+  color: string;
+  year: number;
+  image: string;
+  mileage: number;
+  model: string;
+  transmission: string;
+  hp: number;
+  carburant: string;
+  rate: number;
 }
-interface CartProps{
-    cartItems:Car[],
-    onRemoveFromCart: (id: number) => void
+interface CartProps {
+  cartItems: Car[],
+  onRemoveFromCart: (id: number) => void
 }
 
-const cart: React.FC<CartProps>=({cartItems,onRemoveFromCart})=>{
-    return (
-        <div>
-          <h2>Shopping List!</h2>
-          {cartItems?.map((car)=>
-          <div key={car.id} className='cart-item'>
-            <img src={car.image} alt={car.brand}/>
-            <div>
+
+
+const cart: FC<CartProps> = ({ cartItems, onRemoveFromCart }) => {
+  const [amount, setAmount] = useState<string>("7000") //!amount is 7000 just for testing. when you finish working change it to useState<string>("") and setAmount(Total) total mta3 l products lkol !!
+
+
+  const handleCheckout = () => {
+    axios.post("http://localhost:5000/flouci/payment", { amount: Number(amount) })
+      .then((res: AxiosResponse) => {
+        const { result } = res.data
+        console.log(result);
+
+        window.location.replace(result.link)
+      })
+      .catch((err: AxiosError) => console.log(err))
+  }
+
+  return (
+    <div>
+      <h2>Shopping List!</h2>
+      {cartItems?.map((car) =>
+        <div key={car.id} className='cart-item'>
+          <img src={car.image} alt={car.brand} />
+          <div>
             <h3> Brand : {car.brand}</h3>
             <p>Category : {car.category}</p>
             <p>Year : {car.year}</p>
@@ -41,14 +59,15 @@ const cart: React.FC<CartProps>=({cartItems,onRemoveFromCart})=>{
             <p>Only At: ${car.price}</p>
           </div>
           <div className="buttons">
-          <button onClick={() => onRemoveFromCart(car.id)}>Remove</button>
+            <button onClick={() => onRemoveFromCart(car.id)}>Remove</button>
+          </div>
         </div>
-        </div>
-          )}
-        
-        </div>
-    )
+      )}
+
+      <button onClick={handleCheckout}>Checkout</button>//! only change the button css the functionalities work
+    </div>
+  )
 }
-  
+
 
 export default cart
