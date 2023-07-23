c94cd0fca66374ee3b2c0f3eeb5f5a3f88c07c0b
Salami <medselimthabet2@gmail.com>
2023-07-23 23:20:35 +0100

    added notif toast on login

---
+++ bmw/app/test/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const App = () => {
+    // const notify = () => toast("Wow so easy!");
+    const notify = () => toast.error("Error")
+    return (
+        <div>
+            <button onClick={() => notify()}>Notify!</button>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+            {/* Same as */}
+            <ToastContainer />
+        </div>
+    );
+}
+export default App
